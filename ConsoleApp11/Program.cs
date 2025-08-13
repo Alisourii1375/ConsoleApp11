7f91896bf2b6c4dde3f85f2ed0c2e55762218482
Ali Souri <alisourii1375@gmail.com>
2025-08-13 00:10:58 -0700

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,37 @@
+﻿using ArghamNegarLibrary;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+Console.WriteLine("Please Enter Number 1");
+double X = double.Parse(Console.ReadLine());
+Console.WriteLine("Please Enter Number2");
+Double g = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Please Enter operator");
+char op = char.Parse(Console.ReadLine());
+if (op == '*')
+{
+    var R = calssmath.zarb(X, g);
+    Console.WriteLine(R);
+}
+else if (op == '/')
+{
+    var R = calssmath.taqsim;
+    Console.WriteLine(R);
+}
+else if (op == '+')
+{
+    var R = calssmath.jam(X, g);
+    Console.WriteLine(R);
+}
+else if (op == '-')
+{
+    var R = calssmath.minees(X, g);
+    Console.WriteLine(R);
+
+
+}
+
+
+
+
